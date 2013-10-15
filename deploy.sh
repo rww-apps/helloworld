b1df3fb0f9ce32cdd96551f5d750eb7b7eb41d7e
Melvin Carvalho <melvincarvalho@gmail.com>
2013-10-15 11:36:08 +0200

    first release

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+CERTCMD=""
+
+# get target URI
+read -p "Please provide the URI of the target dir (ex: http://example.org/apps/myapp/): " HOST
+if [ "$HOST" = "" ]
+then
+    read -p "Please provide the URI of the target dir (ex: http://example.org/apps/myapp/): " HOST
+fi
+
+# get certificate path
+read -p "Please provide the path containing the name of your certificate file, in PEM format (leave blank if none): " CERT
+
+# add trailing slash to target dir if not present
+if [ `echo "$HOST" | grep "[^/]$"` ]; then HOST="$HOST/"; fi
+
+# add cert arguments to the curl command
+if [ "$CERT" != "" ]; then CERTCMD="-E $CERT";fi
+
+# create the dir structure (using MKCOL to increase interoperabilty)
+for dir in `find . -mindepth 1 -type d ! -path ./.git\* | sed "s|^\./||"`:
+do
+    echo "Creating dir: $dir"
+    curl -X 'MKCOL' $CERTCMD $HOST$dir
+done
+
+# upload the files now
+for file in `find . -mindepth 1 -type f ! -path ./.git\* | sed "s|^\./||"`:
+do
+    # remove trailing ":" (weird)
+    if [ ! `echo "$file" | grep "[^:]$"` ]; then file="${file%?}";fi
+
+    echo "Uploading file: $HOST$file"
+    curl --upload-file $file $CERTCMD $HOST$file
+done
